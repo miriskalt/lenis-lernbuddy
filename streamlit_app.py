93f96805b246c88b48893c6a8cb2dcde67f2ecdb
miriskalt <56968935+miriskalt@users.noreply.github.com>
2026-06-24 13:39:59 +0000

    input minutes added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🌱 Leni's Lernpflanze")
 st.write(
     "Lass deine Pflanze wachsen, indem du lernst!"
 )
+
+
+minutes_added = st.number_input(
+    "Wie viele Minuten hast du gelernt?",
+    min_value=1,
+    max_value=300,
+    value=25,
+)
